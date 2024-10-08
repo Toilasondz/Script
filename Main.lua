abb64ed956823a7c161debde23c24bcf28cd0be0
TurboLite <168972472+TurboLite@users.noreply.github.com>
2024-10-08 13:46:40 +0700

    Add files via upload

--- Main.lua
+++ Main.lua
@@ -1671,32 +1671,61 @@ LPH_JIT_MAX(function()
 end)()
 
 local Section = Tabs.Main:AddSection("Main Setup Setting Farm")
-local SelectFastAttackMode = (SelectFastAttackMode or "Siêu Đánh Nhanh")
-
-SelectedFastAttackMode = {"Normal Attack","Fast Attack","Siêu Đánh Nhanh"}
-
-local function ChangeModeFastAttack(SelectFastAttackMode)
-	if SelectFastAttackMode == "Normal Attack" then
-		FireCooldown = 0.1
-	elseif SelectFastAttackMode == "Fast Attack" then
-		FireCooldown = 0.07
-	elseif SelectFastAttackMode == "Siêu Đánh Nhanh" then
-		FireCooldown = 0.02
-	end
-end
-
-local SelectedFastAttackModes = Tabs.Main:AddDropdown("SelectedFastAttackModes", {
-	Title = "Chọn Tốc Độ Đánh",
-	Values = SelectedFastAttackMode,
-	Multi = false,
-	Default = 3,
+Tabs.Main:AddButton({
+    Title = "Siêu Pro Fix Lag | Booster",
+    Description = "Giảm Độ Họa Để Chơi Mượt Hơn Thoii",
+    Callback = function()
+        local function FPSBooster()
+            local decalsyeeted = true
+            local g = game
+            local w = g.Workspace
+            local l = g.Lighting
+            local t = w.Terrain
+            
+            sethiddenproperty(l, "Technology", Enum.Technology.Compatibility)
+            sethiddenproperty(t, "Decoration", false)
+            
+            t.WaterWaveSize = 0
+            t.WaterWaveSpeed = 0
+            t.WaterReflectance = 0
+            t.WaterTransparency = 0
+            
+            l.GlobalShadows = false
+            l.FogEnd = 9e9
+            l.Brightness = 0
+            
+            settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
+            
+            for _, v in pairs(g:GetDescendants()) do
+                if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+                    v.Material = Enum.Material.Plastic
+                    v.Reflectance = 0
+                elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+                    v.Transparency = 1
+                elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+                    v.Lifetime = NumberRange.new(0)
+                elseif v:IsA("Explosion") then
+                    v.BlastPressure = 1
+                    v.BlastRadius = 1
+                elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+                    v.Enabled = false
+                elseif v:IsA("MeshPart") then
+                    v.Material = Enum.Material.Plastic
+                    v.Reflectance = 0
+                    v.TextureID = 10385902758728957 -- You might want to verify this TextureID
+                end
+            end
+            
+            for _, e in pairs(l:GetChildren()) do
+                if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+                    e.Enabled = false
+                end
+            end
+        end
+        
+        FPSBooster()
+    end
 })
-
-SelectedFastAttackModes:OnChanged(function(value)
-	SelectFastAttackMode = value
-	ChangeModeFastAttack(SelectFastAttackMode)	
-end)
-
 local FASTAT = Tabs.Main:AddToggle("Fast_Attack", {Title = "Fast Attack", Default = true })
 FASTAT:OnChanged(function(value)
 	Fast_Attack = value
@@ -2016,7 +2045,31 @@ end)
 
 ---//Farm
 local Section = Tabs.Main:AddSection("Main Auto Farm")
+local SelectFastAttackMode = (SelectFastAttackMode or "Siêu Đánh Nhanh")
 
+SelectedFastAttackMode = {"Normal Attack","Fast Attack","Siêu Đánh Nhanh"}
+
+local function ChangeModeFastAttack(SelectFastAttackMode)
+	if SelectFastAttackMode == "Normal Attack" then
+		FireCooldown = 0.1
+	elseif SelectFastAttackMode == "Fast Attack" then
+		FireCooldown = 0.07
+	elseif SelectFastAttackMode == "Siêu Đánh Nhanh" then
+		FireCooldown = 0.02
+	end
+end
+
+local SelectedFastAttackModes = Tabs.Main:AddDropdown("SelectedFastAttackModes", {
+	Title = "Chọn Tốc Độ Đánh",
+	Values = SelectedFastAttackMode,
+	Multi = false,
+	Default = 3,
+})
+
+SelectedFastAttackModes:OnChanged(function(value)
+	SelectFastAttackMode = value
+	ChangeModeFastAttack(SelectFastAttackMode)	
+end)
 local Dropdown = Tabs.Main:AddDropdown("DropdownFarm", {
     Title = "Chọn Farm",
     Values = {"Farm Level", "Farm Bone", "Farm Katakuri"},
